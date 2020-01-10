57b6a133b5f51b2bffc7f09b75c4f76304a0cb00
Emiliano Montesdeoca <emimontesdeoca@outlook.com>
2020-01-10 18:41:29 +0000

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -17,7 +17,7 @@ Remove-Item bin\Release\*.nupkg
 
 dotnet pack -c Release
 
-.nuget\nuget push *.nupkg -Source "https://nuget.org"
+.nuget\nuget push bin\Release\*.nupkg -Source "https://nuget.org"
 
 Remove-Item bin\Release\*.nupkg
 
